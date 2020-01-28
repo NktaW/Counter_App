78036d0dbba04c3935e341fdfa29b8d038502168
NktaW <NktaW@users.noreply.github.com>
2020-01-28 14:22:01 +0200

    Updates

---
+++ precache-manifest.93383f64439ca67a88c94fc57d941773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49594beea7e479e5163900fc64dd11e",
+    "url": "/Counter_App/index.html"
+  },
+  {
+    "revision": "dd8038bb3c02b5f65b2c",
+    "url": "/Counter_App/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "31a1127ec343cfefdeca",
+    "url": "/Counter_App/static/css/main.ca06edb6.chunk.css"
+  },
+  {
+    "revision": "dd8038bb3c02b5f65b2c",
+    "url": "/Counter_App/static/js/2.6f8b5f76.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Counter_App/static/js/2.6f8b5f76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31a1127ec343cfefdeca",
+    "url": "/Counter_App/static/js/main.293e4748.chunk.js"
+  },
+  {
+    "revision": "b92ce0da57d35a2a9eac",
+    "url": "/Counter_App/static/js/runtime-main.08d93c45.js"
+  }
+]);
